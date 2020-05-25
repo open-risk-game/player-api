2db259897851794657c3f8f222906b6e0b3b4a9d
Adam Blackwater <me@adamblackwater.com>
2020-05-25 09:35:30 +0100

    Add get player

--- player.py
+++ player.py
@@ -22,4 +22,16 @@ class Player:
         return web.Response(text='Player created')
 
     async def get(request):
+        player_id = request.rel_url.query['id']
+        async with request.app['pool'].acquire() as db_conn:
+            cursor = await db_conn.cursor(aiomysql.DictCursor)
+            query = f'''
+            SELECT *
+            FROM players
+            WHERE id = {player_id}
+            '''
+            if await cursor.execute(query) == 0:
+                response = {'error': 'player not created'}
+                return web.Response(response)
+            await db_conn.commit()
         return web.json_response(Player('abc').__dict__)
