9044fb3fb4479e7a28fab63defd811d737bdfa74
Adam Blackwater <me@adamblackwater.com>
2020-06-19 11:33:07 +0100

    General housekeeping

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # player-api
+
+For handling player actions.
+
+## Development
+
+Test against your local `MySQL` instance. See the `game-database` repository
+for latest database schema and test data. The username and password needed for
+you database is in the `Makefile`. Don't forget to give your database user full
+privileges or you may have trouble running the tests.
+
+## Docker
+
+> docker build -t player-api .
+
+You only need to build this image if you are working on the `game-service`
+repository.
--- app.py
+++ app.py
@@ -3,14 +3,20 @@ import aiomysql
 from aiohttp import web
 from player import Player
 
+DB_HOST = os.environ.get('DB_HOST')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+DB_NAME = os.environ.get('DB_NAME')
+DB_PORT = int(os.environ.get('DB_PORT'))
+
 
 async def create_db_pool(app):
     app['pool'] = await aiomysql.create_pool(
-            host=os.environ.get('DB_HOST'),
-            user=os.environ.get('DB_USER'),
-            password=os.environ.get('DB_PASS'),
-            db=os.environ.get('DB_NAME'),
-            port=int(os.environ.get('DB_PORT'))
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME,
+            port=DB_PORT
         )
 
 app = web.Application()
